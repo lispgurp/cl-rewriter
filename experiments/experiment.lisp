604eefaee4e0c25cfa4835b4da684a63dfab9907
Gurpreet Saini <geezsaini@yahoo.com>
2013-02-15 12:50:11 -0800

    experiment mods

--- experiments/experiment.lisp
+++ experiments/experiment.lisp
@@ -1,14 +1,3 @@
-(defun apply-rule-at-string (r str)
-  (format t "applying rule(~A), ~A ~%" (rule-name r) str)
-  (cond   
-    ((null r) nil)
-    ((is-pattern? r)
-     (break-apart-string str :based-on r))
-    (t
-     (loop for sub-rule in (rule-content r) 
-        append
-          (list (apply-rule-at-string sub-rule str))))))
-
 (defun find-rule (name r)
   "traverses the hierarchy, returning the target"
   (cond   
@@ -21,27 +10,55 @@
             (when (not (null result))
               (return result)))))))
 
-(defun find-rule2 (name hierarchy)
+(defun find-rule2 (name r)
   (recur-rule 
-   hierarchy
-   :pattern-fn #'(lambda (rule) (nil)) 
-   :name-fn #'(lambda (rule) (r))
+   name
+   r
+   :pattern-fn #'(lambda (rule) nil) 
+   :name-fn #'(lambda (rule) (rule))
    :child-fn #'(lambda (rule)
-                 ;result = recur-rule with this specification
-                 ;when the result is not null, return from the recur-rule
-                 )))
+                 (let ((result (find-rule2 name rule)))
+                   (when (not (null result))
+                     (return-from find-rule2 result))))
+                 ))
 
-(defun recur-rule (r &key pattern-fn name-fn child-fn)
+(defun apply-rule-at-string (r str)
+  (format t "applying rule(~A), ~A ~%" (rule-name r) str)
+  (cond   
+    ((null r) nil)
+    ((is-pattern? r)
+     (break-apart-string str :based-on r))
+    (t
+     (loop for sub-rule in (rule-content r) 
+        append
+          (list (apply-rule-at-string sub-rule str))))))
+
+(defun apply-rule-to-string (name r str )
+  (format t "applying rule(~A), ~A ~%" (rule-name r) str)
+  (recur-rule
+   name
+   r
+   :pattern-fn #'(lambda (rule)
+                   (let ((results
+                          (break-apart-string str :based-on rule)))
+                     results))
+   :name-fn #'(lambda (rule) nil)
+   :child-fn #'(lambda (rule)
+                 (apply-rule-to-string name rule str))))
+
+
+(defun recur-rule (name r &key pattern-fn name-fn child-fn)
   (cond
     ((null r) nil)
     ((is-pattern? r)
      (funcall pattern-fn r))
-    ((eq (rule-name r) r)
+    ((eq (rule-name r) name)
      (funcall name-fn r))
     (t
      (loop for rule in (rule-content r)
           do (funcall child-fn r)))))
           
-          
-          
+;TODO
+; Is return-from doing, what I think it is doing???
+; new method - apply-hierarchy to string           
          
