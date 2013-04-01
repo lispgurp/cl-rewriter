d9b2cd84398b339ce283d27e4ccc9d044454c987
Gurpreet Saini <geezsaini@yahoo.com>
2013-04-01 13:03:35 -0700

    took out design comments
    thanks go to R.H.

--- core/parser.lisp
+++ core/parser.lisp
@@ -1,22 +1,3 @@
-; parser.lisp - Gobbles up blub code (e.g. known as snarfing) and generates a Lisp AST as an s-expression. 
-; The idea is that noisy blub code (even if it is well factored) 
-; is much easier to grok and refactor inside a interactive reprogrammable environment like a REPL 
-; instead of the traditional "manual" ways that developers try to understand/refactor code in a blub 
-; context.
-;
-; *assumption: code being read in compiles (e.g. syntactically valid statements!). if there is serious errors (to define)
-; then tell the user, from the prespective of snarfing, what the problem is. But don't try to validate upfront
-;
-; *I reserve the right to change the parsing technique - right now I am going to iteratively use regexp and stream
-; reading. If this is too much of a pain/boring.  I can use an open source or proprietary parsing engine.
-; for the moment this is fun enough! :). Some possibilities 
-;   Proprietary "Kitchen Sink" implementor 
-;   Roll my own "Natural Language" Parser as per Kitchen Sink
-;   FFI/Interop with  
-;       1. some welll known C library used for parsing (lexx/yacc)
-;       2. easy to use .net library F# (Irony)
-;       3. easy to use java library Clojure (ANTLR)
-
 (ql:quickload "cl-ppcre")
 
 ;(defpackage :cl-rewriter
